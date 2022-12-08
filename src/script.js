d4ad50bd1fb1b07abbf90211700ae6b7395835c3
shalalajun <shalalajun@naver.com>
2022-12-08 21:50:59 +0900

    second
    second

--- src/script.js
+++ src/script.js
@@ -2,6 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
+import { TetrahedronGeometry } from 'three'
 
 /**
  * Base
@@ -19,15 +20,61 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const particleTexture = textureLoader.load('/textures/particles/2.png')
 
 /**
- * Test cube
+ * Geometry
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
+
+ const particleGeometry = new THREE.BufferGeometry(1, 32, 32)
+
+const count = 60000
+
+const positions = new Float32Array(count * 3)
+//const colors = new Float32Array(count * 3)
+
+for(let i=0; i<count * 3; i++)
+{
+    positions[i] = (Math.random() - 0.5) * 10
+    //colors[i] = Math.random()
+}
+
+
+
+particleGeometry.setAttribute(
+    'position',
+    new THREE.BufferAttribute(positions, 3)
 )
-scene.add(cube)
+
+// particleGeometry.setAttribute(
+//     'color',
+//     new THREE.BufferAttribute(colors, 3)
+// )
+/**
+ * particles
+ */
+
+const particleMaterial = new THREE.PointsMaterial({
+    size: 0.1,
+    sizeAttenuation: true,
+    color: new THREE.Color('#ff88cc'),
+    transparent: true,
+    alphaMap : particleTexture,
+    //alphaTest : 0.001,
+    //depthTest : false,
+    depthWrite : false,
+    blending : THREE.AdditiveBlending,
+   // vertexColors : true
+})
+
+/**
+ * Points
+ */
+
+const particles = new THREE.Points(particleGeometry, particleMaterial)
+scene.add(particles);
+
+
 
 /**
  * Sizes
@@ -82,6 +129,24 @@ const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
+    for(let i=0; i< count; i++)
+    {
+        const i3 = i*3
+
+        const x = particleGeometry.attributes.position.array[i3]
+        if(x == count)
+        {
+            x =0;
+            z += 1;
+        }
+
+        
+        const z = particleGeometry.attributes.position.array[i3+2]
+        
+        particleGeometry.attributes.position.array[i3 + 1] = multiWave(x, z, elapsedTime);
+    }
+
+    particleGeometry.attributes.position.needsUpdate = true
     // Update controls
     controls.update()
 
@@ -91,5 +156,22 @@ const tick = () =>
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
 }
+
+ function multiWave(x, z, t)
+    {
+        let my = Math.sin(Math.PI * ((0.2*x) + (0.1*z) + t));
+         my += Math.sin(2* Math.PI * ((0.2*z) + t));
+         my += Math.sin(Math.PI * ((x + z) * 0.2  + t));
+        return my * (1.5 / 2.5);
+    }
+
+ function ripple(x, z, t)
+    {
+        let d = Math.abs(x * x + z * z) * 0.25;
+        let ry = Math.sin(Math.PI * (2 * d - t));
+        return ry / (1 + 2 * d);
+    }
+
+
 
 tick()
